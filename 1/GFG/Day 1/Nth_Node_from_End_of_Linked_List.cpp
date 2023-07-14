7f623cfcb6aeda4b2148b0a5dd6bbd28bac5702a
Saiteja-1234 <saiteja.miriyala01@gmail.com>
2023-07-14 22:09:23 +0530

    day 1

---
+++ 1/GFG/Day 1/Nth_Node_from_End_of_Linked_List.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    int getNthFromLast(Node *head, int n)
+    {
+           // Your code here
+           Node *c = head,*N = head;
+           while(n){
+               if(!c) return -1;
+               c = c->next;
+               n--;
+           }
+           while(c){
+               c= c->next;
+            N = N->next;
+           }
+           if(N) return N->data;
+           return -1;
+    }
+};
